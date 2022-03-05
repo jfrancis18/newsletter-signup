458694d10ba1908179792e8879307e27ec177a1a
Juanita Francis <100166085+jfrancis18@users.noreply.github.com>
2022-03-04 19:40:26 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { path } = require('express/lib/application');
+const { HTTPRequest } = require('puppeteer');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://127.0.0.1:27017';
+
+
+app.use(express.static('images'))
+
+
+
+//setup connection
+MongoClient.connect(url, {useUnifiedTopology: true})
+.then(client => {
+    console.log('connected to database');
+    const db = client.db('user-signup-info');
+    const manateeCollection = db.collection('manatees');
+
+
+
+//----------middleware------------
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+
+//----------routes----------------
+//going to index.ejs and reading database entries
+app.get('/', (req, res) =>{
+    db.collection('manatees').find().toArray()
+    .then(manatees => {
+        res.render('index.ejs', {manatees: manatees})
+    })
+    .catch(/*---*/)
+})
+
+//grabbing form data and adding to database
+app.post('/manatees', (req, res)=>{
+    //console.log(req.body);
+    manateeCollection.insertOne(req.body)
+    .then(result =>{
+        //console.console.log(result);
+        res.redirect('/');
+    })
+    .catch(error => console.error(error));
+})
+
+
+//----------server----------------
+app.listen(3000, function(){
+    console.log('server is running');
+})
+})
+
+//----------end of connection-----
+.catch(console.error);
+
